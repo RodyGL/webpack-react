ce1fe11e48df9ff25c6c9f4dca2c017ae2053d2c
RodyGL <rody.guzman@outlook.com>
2020-09-08 14:05:18 -0600

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
   extends: [
     'react-app',
+    'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
     'prettier/react',
+  ],
+  plugins: [
+    'jsx-a11y', //
   ],
 };
